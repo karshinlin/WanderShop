37e309bffce39afaf9e6d43c78a3d41f85368523
Kar Shin Lin <lin.karshin@gmail.com>
2019-02-24 15:35:34 -0500

    Added login and create account functionality.
    React Native hooks into Server.py API calls to log into and create accounts in Firebase.

--- Server/server.py
+++ Server/server.py
@@ -20,13 +20,13 @@ def login_page():
     error = ''
     try:
         if request.method == "POST":
-		
-            email = request.values['email']
-            password = request.values['password']
-
+            jsonReq = request.get_json()
+            print (jsonReq)
+            email = jsonReq['email']
+            password = jsonReq['password']
             body = {'email': email, 'password': password}
             params = {'key' : config.FIREBASE_API_KEY}
-
+            
             resp = requests.request('post', _verify_password_url, params=params, json=body)
             if bool(resp.json().get('registered')):
                 user = auth.get_user_by_email(email)
@@ -39,6 +39,8 @@ def login_page():
 
     except Exception as e:
         #flash(e)
+        print (e)
+        print ("error ")
         return json.jsonify({'success':0})
 
 @app.route('/register/', methods=["POST"])
@@ -47,25 +49,24 @@ def register_page():
     error = ''
     try:
         if request.method == "POST":
-		
-            name = request.values['name']
-            email = request.values['email']
-            phone = request.values['phone']
-            password = request.values['password']
+            jsonReq = request.get_json()
+            name = jsonReq['name']
+            email = jsonReq['email']
+            phone = jsonReq['phone']
+            password = jsonReq['password']
 
             user = auth.create_user(display_name=name, email=email, phone_number=phone, password=password)
-
             token = auth.create_custom_token(user.uid)
 
-
-
-            return json.jsonify({'success':1, 'token': token, 'email': email, 'name': name, 'phone': phone})
+            return json.jsonify({'success':1, 'token': token.decode('utf-8'), 'email': email, 'name': name, 'phone': phone})
         return json.jsonify({'success':0})
 
 
     except Exception as e:
         #flash(e)
+        print (e)
+        print ("error")
         return json.jsonify({'success':0})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- WanderShopApp/App.js
+++ WanderShopApp/App.js
@@ -1,49 +1,263 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+import React from "react";
+import { Button, View, Text, StyleSheet, TextInput } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-
-export default class App extends Component {
+class WelcomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello World!</Text>
-        <Button title="Press Me"></Button>
+		<View style={styles.container}>
+      <View style={{flex:.2}}></View>
+      <View style={{flex:.4}}>
+        <Text style={styles.welcome}>WanderShop</Text>
+        <Text style={styles.normal}>Solve all your trip planning needs.</Text>
       </View>
+      <View style={{flex:.4}}>
+        <Button title="Sign In"
+          onPress={() => this.props.navigation.navigate('Login')}/>
+        <Button title="Create Account"
+          onPress={() => this.props.navigation.navigate('CreateAccount')}/>
+      </View>
+		
+		</View>
     );
   }
 }
+class LoginScreen extends React.Component {
+	render() {
+		const {navigate} = this.props.navigation;
+		return (
+			<View style={styles.container}>
+        <View style={{flex:.2, flexDirection: 'column'}}></View>
+        <View style={{flex:.3}}>
+          <Text style={styles.normal}>Please log into your account.</Text>
+        </View>
+        <View style={{flex:.2, flexDirection: 'column'}}>
+          <View style={{flex: 1, flexDirection:'row', justifyContent: 'center'}}>
+            <View style={{flex:.3}}></View>
+            <TextInput
+              style={{flex:.7}}
+              placeholder="Email"
+              onChangeText={(email) => this.setState({email})}>
+            </TextInput>
+          </View>
+          <View style={{flex: 1, flexDirection:'row', justifyContent: 'center'}}>
+            <View style={{flex:.3}}></View>
+            <TextInput
+              style={{flex:.7}}
+              placeholder="Password"
+              secureTextEntry={true}
+              onChangeText={(pass) => this.setState({pass})}>
+            </TextInput>
+          </View>
+          <Button title="Sign In"
+            onPress={() => this.login()}/>
+        </View>
+        <View style={{flex:.4, flexDirection: 'column'}}>
+          <Text style={styles.normal}>Nothing</Text>
+        </View>
+		</View>
+		);
+  };
+  
+  login() {
+    try {
+      console.log(this.state.email)
+      console.log(this.state.pass)
+      fetch('http://127.0.0.1:5000/login/', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+        body: JSON.stringify({
+          email: this.state.email,
+          password: this.state.pass,
+        }),
+      })
+      .then((response) => response.json())
+      .then((responseJSON) => {
+        if (responseJSON['success'] == 1) {
+          this.props.navigation.navigate('Welcome')
+        } else {
+          console.log(responseJSON)
+          console.log("Login failed");
+        }
+      }).catch(function(error) {
+        console.log('There has been a problem with your fetch operation: ' + error.message);
+         // ADD THIS THROW error
+          throw error;
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+// TODO: Provide better error handling (i.e. email already exists)
+// TODO: Provide better validation for phone numbers (must be in format +17705555555)
+//       can transform afterwards or just prompt user 
+class CreateAccountScreen extends React.Component {
+	render() {
+    const {navigate} = this.props.navigation;
+		return (
+			<View style={styles.container}>
+        <View style={{flex:.2, flexDirection: 'column'}}></View>
+        <View style={{flex:.3}}>
+          <Text style={styles.normal}>Please create an account.</Text>
+        </View>
+        <View style={{flex:.3, flexDirection: 'column'}}>
+          <View style={{flex: 1, flexDirection:'row', justifyContent: 'center'}}>
+            <View style={{flex:.3}}></View>
+            <TextInput
+              style={{flex:.7}}
+              placeholder="Name"
+              onChangeText={(name) => this.setState({name})}>
+            </TextInput>
+          </View>
+          <View style={{flex: 1, flexDirection:'row', justifyContent: 'center'}}>
+            <View style={{flex:.3}}></View>
+            <TextInput
+              style={{flex:.7}}
+              placeholder="Phone"
+              onChangeText={(phone) => this.setState({phone})}>
+            </TextInput>
+          </View>
+          <View style={{flex: 1, flexDirection:'row', justifyContent: 'center'}}>
+            <View style={{flex:.3}}></View>
+            <TextInput
+              style={{flex:.7}}
+              placeholder="Email"
+              onChangeText={(email) => this.setState({email})}>
+            </TextInput>
+          </View>
+          <View style={{flex: 1, flexDirection:'row', justifyContent: 'center'}}>
+            <View style={{flex:.3}}></View>
+            <TextInput
+              style={{flex:.7}}
+              placeholder="Password (at least 8 characters)"
+              secureTextEntry={true}
+              onChangeText={(password) => this.setState({password})}>
+            </TextInput>
+          </View>
+          <View style={{flex: 1, flexDirection:'row', justifyContent: 'center'}}>
+            <View style={{flex:.3}}></View>
+            <TextInput
+              style={{flex:.7}}
+              placeholder="Confirm Password"
+              secureTextEntry={true}
+              onChangeText={(confirmPass) => this.setState({confirmPass})}>
+            </TextInput>
+          </View>
+          <Button title="Create Account"
+            onPress={() => {
+              this.verifyPasswordMatch();
+              this.forceUpdate(this.createAccount());
+            }}/>
+        </View>
+        <View style={{flex:.5, flexDirection: 'column'}}>
+          <Text style={styles.normal}>Nothing</Text>
+        </View>
+		</View>
+		);
+  };
+  
+  verifyPasswordMatch() {
+    if (this.state.password == this.state.confirmPass) {
+      console.log(this.state.password + " " + this.state.confirmPass);
+      this.setState((state) => {
+        return {passwordVerified: true};
+      });
+      //this.setState({passwordVerified: true}, this.createAccount());
+      console.log(this.state.passwordVerified);
+    } else {
+      this.setState((state) => {
+        return {passwordVerified: false};
+      });
+    }
+  }
+
+  createAccount() {
+    if (!this.state.passwordVerified) {
+      console.log(this.state.passwordVerified);
+      return;
+    }
+    try {
+      fetch('http://127.0.0.1:5000/register/', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+
+        body: JSON.stringify({
+          name: this.state.name,
+          phone: this.state.phone,
+          email: this.state.email,
+          password: this.state.password,
+        }),
+      })
+      .then((response) => response.json())
+      .then((responseJSON) => {
+        if (responseJSON['success'] == 1) {
+          this.props.navigation.navigate('Welcome')
+        } else {
+          console.log(responseJSON)
+          console.log("Create account failed");
+        }
+      }).catch(function(error) {
+        console.log('There has been a problem with your fetch operation: ' + error.message);
+         // ADD THIS THROW error
+          throw error;
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+const AppNavigator = createStackNavigator({
+	Welcome: WelcomeScreen,
+	Login: LoginScreen,
+	CreateAccount: CreateAccountScreen
+}
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
+  
+  const styles = StyleSheet.create({
+	container: {
+	  flex: 1,
+	  justifyContent: 'center',
+	  backgroundColor: '#42cef4',
+	  flexDirection: 'column'
+	},
+	welcome: {
+	  fontSize: 40,
+	  textAlign: 'center',
+	  margin: 10,
+	},
+	normal: {
+	  fontSize: 20,
+	  textAlign: 'center',
+	  margin: 10,
+	},
+	instructions: {
+	  textAlign: 'center',
+	  color: '#333333',
+	  marginBottom: 5,
+	},
+	background: {
+	  backgroundColor: '#42cef4',
+	  flex: 1,
+	},
+  });
