9e1b86e8b1205f42f4ff4436758c1d7cfc7caf53
Colby Tobin <>
2019-02-27 00:20:56 -0500

    sample for how to call api for flights

--- WanderShopApp/CartButton.js
+++ WanderShopApp/CartButton.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { TouchableHighlight } from "react-native";
+import { TouchableOpacity } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 class CartButton extends Component {
@@ -11,9 +11,9 @@ class CartButton extends Component {
 
     render() {
 
-        return <TouchableHighlight style={{ paddingRight: 10 }} onPress={this.onPressButton}>
+        return <TouchableOpacity style={{ marginRight: 10 }} onPress={this.onPressButton}>
                 <Icon name="shopping-cart" size={25} color="#42cef4" />
-            </TouchableHighlight>;
+            </TouchableOpacity>;
     }
 }
 
--- WanderShopApp/FlightView.js
+++ WanderShopApp/FlightView.js
@@ -1,17 +1,62 @@
 import React, { Component } from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, ActivityIndicator } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 class FlightView extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      loading: false,
-      data: [{title:'Flight 1'}, {title:'Flight 2'}, {title: 'Flight 3'}],
+        isLoading: true,
+        error: false,
+        data: [{title:'Flight 1'}, {title:'Flight 2'}, {title: 'Flight 3'}],
+        refreshing: false,
+        time: 30,
     };
   }
 
+  fetchFlights(){
+    return fetch('http://127.0.0.1:5000/getflights/')
+        .then((response) => response.json())
+        .then((response) => {
+            this.setState({
+                isLoading: false,
+                error: false,
+                data: response.flights,
+                refreshing: false,
+                time: 30,
+            }, function () {
+
+            });
+        })
+        .catch((error) => {
+            this.setState({
+                isLoading: false,
+                error: true
+            })
+        });
+    }
+    componentDidMount() {
+        //this.fetchFlights();
+        this.setState({ isLoading: false });
+    }
+
   render() {
+    if (this.state.error) {
+        return (
+            <View style={{ flex: 1, paddingTop: 25, alignSelf: "center" }}>
+                <Icon name="error" size={75} color="#F00" />
+                <Text style={{ alignSelf: "center", color: "#F00" }}>Error</Text>
+            </View>
+            );
+    }
+    if (this.state.isLoading) {
+        return (
+            <View style={{ flex: 1, paddingTop: 25 }}>
+                <ActivityIndicator size="large" color="#0000ff" />
+            </View>
+        );
+    }
     return (
       <View style={{ flex: 1 }}>
         <FlatList
@@ -20,6 +65,8 @@ class FlightView extends Component {
           ({ item }) =>
             <Text style={{ padding: 15, width: '100%' }}>{item.title}</Text>
         }
+        refreshing={this.state.refreshing}
+        onRefresh={this.handleRefresh}
       />
       </View>
     );
